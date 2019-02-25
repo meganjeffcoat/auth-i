1d490c9ae9eb0e743fdb354e58df55ee1af273db
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-25 16:48:38 -0600

    GET written for /users and restricted added to protect endpoint

--- users/usersRoute.js
+++ users/usersRoute.js
@@ -43,4 +43,33 @@ router.post('/login', (req, res) => {
         });
 });
 
+function restricted(req, res, next) {
+    const { username, password } = req.headers;
+    if (username && password) {
+    
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                next();
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'You shall not pass' });
+        })   
+    } else {
+        res.status(400).json({ message: 'Please login or register to continue' })
+    }
+}
+
+router.get('/users',restricted, (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
+});
+
   module.exports = router;
