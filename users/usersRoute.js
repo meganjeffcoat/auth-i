adfa7a07573e952042c35f8f4aaca14bce1d138d
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-25 15:50:17 -0600

    POST request created for register and works correctly and password is NOT plain text

--- users/usersRoute.js
+++ users/usersRoute.js
@@ -9,7 +9,21 @@ const db = require('../data/dbConfig.js');
 const Users = require('../users/users-module.js');
 
 router.get('/', (req, res) => {
-    res.send('Its alive!');
+    res.send('A long time ago in a galaxy far, far away....');
+});
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 15);
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
 });
 
   module.exports = router;
